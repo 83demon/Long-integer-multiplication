21fa671f851b4517d455027c7c2e5f1180ad4536
vo1ku <vkuzko007@gmail.com>
2020-10-11 01:32:54 +0300

    Working version of Multiply classes hierarchy. Stress test is freezed.

--- main.cpp
+++ main.cpp
@@ -1,43 +1,24 @@
 #include <iostream>
-#include "long_int.h"
-#include <vector>
-#include <algorithm>
-#include "vector_tools.h"
+#include "multiply.h"
 
-int len_base(std::vector<int> a, std::vector<int> b){
-    //seeks for the min len of base
-    int res;
-    bool equals_a = equal_elems(a), equals_b = equal_elems(b);
-    if(equals_a && equals_b && a[0]==b[0] &&a[0]==0){
-        res = std::min(a.size(),b.size());
-    }
-    else if(equals_a && a[0]==0){
-        res = b.size();
-    }
-    else if(equals_b && b[0]==0){
-        res = a.size();
-    }
-    else{
-        res = std::min(a.size(),b.size());
-    }
-    return res;
-}
+
+
 
 int main() {
-    LongIntMult::set_base(10000); //no more, than 10 000
-    LongIntMult::set_mult("Karatsuba");
+    LongIntMult::set_base(10); //no more, than 10 000
+    LongIntMult::set_mult(new Karatsuba);
 
 
     LongIntMult a,b,c;
-    a = "510";
-    b = 420;
+    a = "864684";
+    b = 448949815;
 
 
-    c.stress_test(3000);
+    //c.stress_test(3000);
 
 
-    //c = a*b;
-    //std::cout<<c;
+    c = a*b;
+    std::cout<<c;
 
 
     return 0;
